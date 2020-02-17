7ab6db4c0b2964f45a7e1b1d3940c03babd2aa9d
Tarun Sarpanjeri <tarun.sarpanjeri@gmail.com>
2020-02-17 16:12:26 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function app()
+{
+     const Total=parseFloat(document.querySelector('#total').value)
+     const Rate=parseFloat(document.querySelector('#rate').value)
+
+     console.log(Rate);
+     
+     const Tip=Total*(Rate/100)
+     const TaxRate=5.5
+     const Tax=Total*(TaxRate/100)
+     const GrandTotal=Total+Tip+Tax;
+
+     document.querySelector('#Tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#GrandTotal').innerHTML=`$${GrandTotal.toFixed(2)}`
+}
+     document.querySelector('#calci').addEventListener('click',app)
